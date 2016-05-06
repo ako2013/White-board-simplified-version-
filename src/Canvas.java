c85019a56a9f964c58b27854fed494a353bc9751
hpboy95 <hpboy99@gmail.com>
2016-05-06 15:40:15 -0700

    Initial canvas and Whiteboard code
    Deleted all button code. Doesnt work w/o canvas being done QQ

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,4 +1,12 @@
+import javax.swing.JPanel;
 
-public class Canvas {
-
+public class Canvas extends JPanel
+{
+	private final int DEFAULT_LENGTH = 400; 
+	private final int DEFAULT_WIDTH =	400;	
+	
+	public Canvas()
+	{
+		setSize(DEFAULT_LENGTH, DEFAULT_WIDTH);
+	}
 }
--- src/Whiteboard.java
+++ src/Whiteboard.java
@@ -1,9 +1,40 @@
-/**
- * 
- * @author HP
- *
- */
-public class Whiteboard 
-{
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Whiteboard extends JFrame
+{
+	final int FRAME_WIDTH = 1000; 
+	final int FRAME_HEIGHT = 1000; 
+	
+	public static void main(String[] args)
+	{
+		//Size of Window
+		final int FRAME_WIDTH = 1000; 
+		final int FRAME_HEIGHT = 1000; 
+				
+		//New Whiteboard Frame
+		JFrame board = new JFrame("Whiteboard");
+		board.setLayout(new BorderLayout());
+		
+		//Set the Canvas and panel
+		Canvas drawPane = new Canvas();
+		JPanel buttonPane = new JPanel(); 
+		
+		//ButtonCode
+		
+		
+		//Add canvas and buttons to the frame
+		board.add(buttonPane);
+		board.add(drawPane);
+		
+		//Generic Swing things
+		board.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		board.setVisible(true);
+	}
+	
+	
+	
 }
